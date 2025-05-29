000e51314097917fd442670769d53fac4df3176d
Faruk Emre İPEK <farukemreipek34@gmail.com>
2025-05-29 15:58:19 +0300

    Add Mongo DB Service

---
+++ TaskManagerWebAPI/Data/MongoDbService.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using ZstdSharp.Unsafe;
+
+namespace TaskManagerWebAPI.MongoDbService
+{
+    public class MongoDbService
+    {
+        private readonly IConfiguration _configration;
+        private readonly IMongoDatabase? _database;
+        public MongoDbService(IConfiguration configuration)
+        {            
+            _configration = configuration;
+
+            var connectionString = _configration.GetConnectionString("DbConnection");
+            var mongoUrl = MongoUrl.Create(connectionString);   
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);           
+        }
+
+        public IMongoDatabase? Database => _database;
+
+    }
+}
---
+++ TaskManagerWebAPI/Entites/Customer.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TaskManagerWebAPI.Entites
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonElement("_id"),BsonRepresentation(BsonType.ObjectId)] 
+        public int Id { get; set; }
+
+        [BsonElement("name"), BsonRepresentation(BsonType.String)]
+        public string? Name { get; set; }
+
+        [BsonElement("email"), BsonRepresentation(BsonType.String)]
+        public string? Email { get; set; }
+    }
+}
